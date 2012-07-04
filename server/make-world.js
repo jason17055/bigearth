7c495b9aa3155ae8d3eebc0fd31c9a5635682fef
Jason Long <jason@long.name>
2012-07-03 20:11:00 -0400

    make-world: upload edge data

--- server/make-world.js
+++ server/make-world.js
@@ -7,40 +7,73 @@ console.log("Creating world "+worldName);
 
 var DB = new(cradle.Connection)().database(worldName);
 
-DB.save('_design/mapcells', {
-	language: 'javascript',
-	views: {
-	all: { map: function(doc) {
+function createDatabase()
+{
+	DB.create(function(err,res) {
+		if (err) {
+			console.log("ERROR", err);
+		} else {
+			console.log("Database created.");
+			createSchema();
+		}
+		});
+}
+
+function createSchema()
+{
+	var remaining = 0;
+	var count = 0;
+	var callback = function(err,res) {
+			if (err) {
+				console.log("ERROR",err);
+			} else {
+				console.log("Schema object "+(++count)+" created.");
+				remaining--;
+				if (remaining == 0)
+					createTerrain();
+			}
+		};
+
+	remaining++;
+	DB.save('_design/cells', {
+		language: 'javascript',
+		views: {
+		all: { map: function(doc) {
 			if (doc._id.match(/^map\/(\d+)$/))
 			{
 				emit(+RegExp.$1, doc);
 			}
-		} }
-	}
-	}, function(err,res) {
-		console.log("DONE");
-		});
-
-DB.create(function(err,res) {
-
-	if (err) {
-		console.log("ERROR", err);
-	} else {
-
-
-	var remaining = 0;
-	var callback = function(err,res) {
-		if (err) {
-			console.log("ERROR",err);
-		} else {
-			remaining--;
+			} },
+		by_coordinates: { map: function(doc) {
+			if (doc._id.match(/^map\/(\d+)$/))
+			{
+				var lat = Math.asin(doc.pt.z);
+				var lon = Math.atan2(doc.pt.y, doc.pt.x);
+				var band = Math.floor(lat / (Math.PI/12));
+				emit([band,lon,lat], doc);
+			}
+			} }
 		}
+		}, callback);
+}
+
+function createTerrain()
+{
+	var remaining = 0;
+	var count = 0;
+	var callback = function(err,res) {
+			console.log("Terrain object "+(++count)+" created.");
+			if (err) {
+				console.log("ERROR",err);
+			} else {
+				remaining--;
+			}
 		};
 
 	var SG = require('../html/sphere-geometry.js');
 	var MG = require('../html/mapgen.js');
 
-	console.log("generating terrain...");
+	console.log("Generating terrain...");
 	var geometry = new SG.SphereGeometry(22);
 	var map = MG.makeMap(geometry);
 	var coords = MG.makeCoords(geometry);
@@ -48,7 +81,6 @@ DB.create(function(err,res) {
 	map.size = geometry.size;
 	MG.generateTerrain(map, coords);
 
-	console.log("uploading cell data");
 	for (var cellIdx = 1, numCells = geometry.getCellCount();
 		cellIdx <= numCells; cellIdx++)
 	{
@@ -58,6 +90,8 @@ DB.create(function(err,res) {
 		var x = {
 		terrain: c.terrain,
 		height: c.height,
+		temperature: c.temperature,
+		moisture: c.moisture,
 		pt: co.pt,
 		neighbors: geometry.getNeighbors(cellIdx)
 		};
@@ -66,10 +100,21 @@ DB.create(function(err,res) {
 		DB.save('map/'+cellIdx, x, callback);
 	}
 
+	for (var eId in map.edges)
+	{
+		var e = map.edges[eId];
+		if (!e.feature)
+			continue;
+
+		remaining++;
+		DB.save('map/'+eId, { feature: e.feature }, callback);
+	}
+
 	remaining++;
 	DB.save('map', {
 		geometry: 'sphere',
 		size: geometry.size
 		}, callback);
 }
-});
+
+createDatabase();
