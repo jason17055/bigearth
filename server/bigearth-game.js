49c1ce878589378629cddf8fc70a1e2d1e133a8b
Jason Long <jason@long.name>
2012-09-08 20:52:25 -0400

    server: make separate module for Settler-related functions

--- server/bigearth-game.js
+++ server/bigearth-game.js
@@ -7,6 +7,7 @@ var Scheduler = require('./bigearth_modules/scheduler.js');
 var city_module = require('./bigearth_modules/city.js');
 var mod_terrain = require('./bigearth_modules/terrain.js');
 var Location = require('../html/location.js');
+var Settler = require('./bigearth_modules/settler.js');
 
 var G = {
 	world: {},
@@ -405,41 +406,6 @@ function fleetActivityError(fleetId, fleet, errorMessage)
 	fleetMessage(fleetId, "Unable to complete orders: " + errorMessage);
 }
 
-function fleetDisbandInCity(fleetId, fleet)
-{
-	var location = fleet.location;
-	var cellId = Location.toCellId(location);
-
-	var cityId = G.terrain.cells[cellId].city;
-	var city = G.cities[cityId];
-	if (!city && G.terrain.cells[cellId].terrain == 'ocean')
-	{
-		// try to find a city in an adjoining cell
-		var nn = BE.geometry.getNeighbors(cellId);
-		for (var i = 0; i < nn.length; i++)
-		{
-			var c = G.terrain.cells[nn[i]];
-			if (c.city)
-			{
-				cityId = c.city;
-				city = G.cities[cityId];
-				break;
-			}
-		}
-	}
-	if (!city)
-	{
-		return fleetActivityError(fleetId, fleet, "No city at this location");
-	}
-
-	if (fleet.population)
-	{
-		city_addWorkersAny(cityId, city, fleet.population);
-	}
-
-	destroyFleet(fleetId, 'disband-in-city');
-}
-
 function allPlayersWhoCanSee(location)
 {
 	var terrainCell = getTerrainLocation(location);
@@ -493,40 +459,6 @@ function destroyFleet(fleetId, disposition)
 		}
 	}
 	delete G.fleets[fleetId];
-}
-
-function tryToBuildCity(fleetId, fleet)
-{
-	if (!fleetHasCapability(fleet, 'build-city'))
-		return fleetCurrentCommandFinished(fleetId, fleet);
-
-	if (fleet.activity == 'build-city')
-	{
-		if (!Location.isCell(fleet.location))
-		{
-			throw new Error("cannot build city on non-cell terrain; don't know what to do");
-		}
-
-		var terrainCell = G.terrain.cells[Location.toCellId(fleet.location)];
-		if (terrainCell.city)
-		{
-			return fleetActivityError(fleetId, fleet, "There is already a city here.");
-		}
-
-		var city = newCity(fleet.location, fleet.owner);
-		var tid = city._id;
-		terrainCell.city = tid;
-		developLand(fleet.location, 'hamlet', 1);
-		updateFleetSight(tid, city);
-
-		setFleetActivityFlag(fleetId, fleet, null);
-		return fleetDisbandInCity(fleetId, fleet);
-	}
-	else
-	{
-		setFleetActivityFlag(fleetId, fleet, 'build-city');
-		fleetCooldown(fleetId, fleet, 5000);
-	}
 }
 
 function moveFleetAlongCoast(fleetId)
@@ -1267,15 +1199,15 @@ function fleetActivity(fleetId)
 	}
 	else if (currentOrder.command == 'build-city')
 	{
-		return tryToBuildCity(fleetId, fleet);
+		return Settler.buildCity(fleetId, fleet, currentOrder);
 	}
 	else if (currentOrder.command == 'auto-settle')
 	{
-		return fleetAutoSettle(fleetId, fleet);
+		return Settler.autoSettle(fleetId, fleet, currentOrder);
 	}
 	else if (currentOrder.command == 'disband')
 	{
-		return fleetDisbandInCity(fleetId, fleet);
+		return Settler.disbandInCity(fleetId, fleet, currentOrder);
 	}
 	else
 	{
@@ -1366,143 +1298,6 @@ function fleetCanSettle(fleet)
 	return fleet.type == 'settler';
 }
 
-function fleetAutoSettle(fleetId, fleet)
-{
-	var seen = {};
-	var curRing = {};
-	var fleetCellId = Location.toCellId(fleet.location);
-	seen[fleetCellId] = true;
-	curRing[fleetCellId] = true;
-
-	var map = G.maps[fleet.owner];
-
-	var bestValue = getSettlementFitness(map, fleetCellId);
-	var best = fleet.location;
-	for (var dist = 0; ; dist++)
-	{
-		var nextRing = {};
-		for (var cid in curRing)
-		{
-			var v = getSettlementFitness(map, cid);
-			if (v > bestValue)
-			{
-				bestValue = v;
-				best = cid;
-			}
-
-			var nn = BE.geometry.getNeighbors(cid);
-			for (var j = 0; j < nn.length; j++)
-			{
-				var nid = nn[j];
-				if (!seen[nid])
-				{
-					seen[nid] = true;
-					nextRing[nid] = true;
-				}
-			}
-		}
-		curRing = nextRing;
-
-		if (dist >= 5 && bestValue >= 3.0)
-		{
-			fleet.orders.shift();
-			fleet.orders.unshift({
-				command: 'build-city'
-				});
-			fleet.orders.unshift({
-				command: 'goto',
-				location: best
-				});
-			return fleetActivity(fleetId, fleet);
-		}
-	}
-
-	return fleetActivityError(fleetId, fleet, "Unable to find a suitable city location.")
-}
-
-function getSettlementFitness(map, cellId)
-{
-	var thisCellFitness = 0;
-
-	var c = map.cells[cellId];
-	if (!c || c.city)
-	{
-		return 0;
-	}
-
-	if (c.terrain == 'grassland' ||
-		c.terrain == 'plains' ||
-		c.terrain == 'forest' ||
-		c.terrain == 'hills')
-	{
-		thisCellFitness = 1;
-	}
-
-	var numGoodNeighbors = 0;
-	var numRivers = 0;
-	var nn = BE.geometry.getNeighbors(cellId);
-	for (var i = 0; i < nn.length; i++)
-	{
-		var n = map.cells[nn[i]];
-		if (!n) continue;
-		if (n.terrain == 'grassland' ||
-			n.terrain == 'plains' ||
-			n.terrain == 'forest' ||
-			n.terrain == 'hills')
-		{
-			numGoodNeighbors++;
-		}
-
-		var eId = BE.geometry._makeEdge(cellId, nn[i]);
-		var e = map.edges[eId];
-		if (e && e.feature == 'river')
-		{
-			numRivers++;
-		}
-	}
-
-	var neighborsFitness = numGoodNeighbors / 5 - 0.3 * Math.abs(numRivers - 2);
-
-	// look up to five cells away for another city
-	var dist = distanceToNearestCity(map, cellId, 5);
-	var distCityFitness = 2 - 4/dist;
-	return thisCellFitness + neighborsFitness + distCityFitness;
-}
-
-function distanceToNearestCity(map, cellId, maxDist)
-{
-	var seen = {};
-	var ring = {};
-	ring[cellId] = true;
-	seen[cellId] = true;
-
-	for (var dist = 0; dist <= maxDist; dist++)
-	{
-		var nextRing = {};
-		for (var cid in ring)
-		{
-			seen[cid] = true;
-			var c = map.cells[cid];
-			if (c && c.city)
-				return dist;
-
-			var nn = BE.geometry.getNeighbors(cid);
-			for (var j = 0; j < nn.length; j++)
-			{
-				var nid = nn[j];
-				if (!seen[nid])
-				{
-					seen[nid] = true;
-					nextRing[nid] = true;
-				}
-			}
-		}
-		ring = nextRing;
-	}
-
-	return maxDist+1;
-}
-
 function getFleetInfoForPlayer(fleetId, playerId)
 {
 	var f = G.fleets[fleetId];
@@ -1526,7 +1321,7 @@ function getFleetInfoForPlayer(fleetId, playerId)
 		if (fleetCanSettle(f))
 		{
 			_fleet.canSettle = true;
-			_fleet.settlementFitness = getSettlementFitness(
+			_fleet.settlementFitness = Settler.getSettlementFitness(
 				G.maps[f.owner], Location.toCellId(f.location));
 		}
 
@@ -1826,4 +1621,10 @@ if (typeof global !== 'undefined')
 	global.updateFleetSight = updateFleetSight;
 	global.developLand = developLand;
 	global.createUnit = createUnit;
+
+	global.fleetHasCapability = fleetHasCapability;
+	global.fleetActivityError = fleetActivityError;
+	global.setFleetActivityFlag = setFleetActivityFlag;
+	global.fleetCooldown = fleetCooldown;
+	global.destroyFleet = destroyFleet;
 }
---
+++ server/bigearth_modules/settler.js
@@ -0,0 +1,212 @@
+var Location = require('../../html/location.js');
+var City = require('./city.js');
+
+function autoSettle(fleetId, fleet, currentOrder)
+{
+	var seen = {};
+	var curRing = {};
+	var fleetCellId = Location.toCellId(fleet.location);
+	seen[fleetCellId] = true;
+	curRing[fleetCellId] = true;
+
+	var map = G.maps[fleet.owner];
+
+	var bestValue = getSettlementFitness(map, fleetCellId);
+	var best = fleet.location;
+	for (var dist = 0; ; dist++)
+	{
+		var nextRing = {};
+		for (var cid in curRing)
+		{
+			var v = getSettlementFitness(map, cid);
+			if (v > bestValue)
+			{
+				bestValue = v;
+				best = cid;
+			}
+
+			var nn = BE.geometry.getNeighbors(cid);
+			for (var j = 0; j < nn.length; j++)
+			{
+				var nid = nn[j];
+				if (!seen[nid])
+				{
+					seen[nid] = true;
+					nextRing[nid] = true;
+				}
+			}
+		}
+		curRing = nextRing;
+
+		if (dist >= 5 && bestValue >= 3.0)
+		{
+			fleet.orders.shift();
+			fleet.orders.unshift({
+				command: 'build-city'
+				});
+			fleet.orders.unshift({
+				command: 'goto',
+				location: best
+				});
+			return fleetActivity(fleetId, fleet);
+		}
+	}
+
+	return fleetActivityError(fleetId, fleet, "Unable to find a suitable city location.")
+}
+
+function getSettlementFitness(map, cellId)
+{
+	var thisCellFitness = 0;
+
+	var c = map.cells[cellId];
+	if (!c || c.city)
+	{
+		return 0;
+	}
+
+	if (c.terrain == 'grassland' ||
+		c.terrain == 'plains' ||
+		c.terrain == 'forest' ||
+		c.terrain == 'hills')
+	{
+		thisCellFitness = 1;
+	}
+
+	var numGoodNeighbors = 0;
+	var numRivers = 0;
+	var nn = BE.geometry.getNeighbors(cellId);
+	for (var i = 0; i < nn.length; i++)
+	{
+		var n = map.cells[nn[i]];
+		if (!n) continue;
+		if (n.terrain == 'grassland' ||
+			n.terrain == 'plains' ||
+			n.terrain == 'forest' ||
+			n.terrain == 'hills')
+		{
+			numGoodNeighbors++;
+		}
+
+		var eId = BE.geometry._makeEdge(cellId, nn[i]);
+		var e = map.edges[eId];
+		if (e && e.feature == 'river')
+		{
+			numRivers++;
+		}
+	}
+
+	var neighborsFitness = numGoodNeighbors / 5 - 0.3 * Math.abs(numRivers - 2);
+
+	// look up to five cells away for another city
+	var dist = distanceToNearestCity(map, cellId, 5);
+	var distCityFitness = 2 - 4/dist;
+	return thisCellFitness + neighborsFitness + distCityFitness;
+}
+
+function distanceToNearestCity(map, cellId, maxDist)
+{
+	var seen = {};
+	var ring = {};
+	ring[cellId] = true;
+	seen[cellId] = true;
+
+	for (var dist = 0; dist <= maxDist; dist++)
+	{
+		var nextRing = {};
+		for (var cid in ring)
+		{
+			seen[cid] = true;
+			var c = map.cells[cid];
+			if (c && c.city)
+				return dist;
+
+			var nn = BE.geometry.getNeighbors(cid);
+			for (var j = 0; j < nn.length; j++)
+			{
+				var nid = nn[j];
+				if (!seen[nid])
+				{
+					seen[nid] = true;
+					nextRing[nid] = true;
+				}
+			}
+		}
+		ring = nextRing;
+	}
+
+	return maxDist+1;
+}
+
+function buildCity(fleetId, fleet, currentOrder)
+{
+	if (!fleetHasCapability(fleet, 'build-city'))
+		return fleetActivityError(fleetId, fleet, "Cannot build city with this unit type");
+
+	if (fleet.activity == 'build-city')
+	{
+		if (!Location.isCell(fleet.location))
+		{
+			throw new Error("cannot build city on non-cell terrain; don't know what to do");
+		}
+
+		var terrainCell = G.terrain.cells[Location.toCellId(fleet.location)];
+		if (terrainCell.city)
+		{
+			return fleetActivityError(fleetId, fleet, "There is already a city here.");
+		}
+
+		var city = newCity(fleet.location, fleet.owner);
+		var tid = city._id;
+		terrainCell.city = tid;
+		developLand(fleet.location, 'hamlet', 1);
+		updateFleetSight(tid, city);
+
+		setFleetActivityFlag(fleetId, fleet, null);
+		return fleetDisbandInCity(fleetId, fleet);
+	}
+	else
+	{
+		setFleetActivityFlag(fleetId, fleet, 'build-city');
+		fleetCooldown(fleetId, fleet, 5000);
+	}
+}
+
+function disbandInCity(fleetId, fleet, currentOrder)
+{
+	var location = fleet.location;
+	var cellId = Location.toCellId(location);
+
+	var cityId = G.terrain.cells[cellId].city;
+	var city = G.cities[cityId];
+	if (!city && G.terrain.cells[cellId].terrain == 'ocean')
+	{
+		// try to find a city in an adjoining cell
+		var nn = BE.geometry.getNeighbors(cellId);
+		for (var i = 0; i < nn.length; i++)
+		{
+			var c = G.terrain.cells[nn[i]];
+			if (c.city)
+			{
+				cityId = c.city;
+				city = G.cities[cityId];
+				break;
+			}
+		}
+	}
+	if (!city)
+	{
+		return fleetActivityError(fleetId, fleet, "No city at this location");
+	}
+
+	if (fleet.population)
+	{
+		city_addWorkersAny(cityId, city, fleet.population);
+	}
+
+	destroyFleet(fleetId, 'disband-in-city');
+}
+
+exports.buildCity = buildCity;
+exports.autoSettle = autoSettle;
+exports.disbandInCity = disbandInCity;
