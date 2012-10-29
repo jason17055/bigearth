a66110cef93ea3ef8f006a44331f14e9f94a06f2
Jason Long <jason@long.name>
2012-10-29 18:07:01 -0400

    java: trying some benchmarking of getNeighbors
    currently getNeighbors() takes about 62 milliseconds (pretty good, I think!)

--- common/bigearth/TestSphereGeometry.java
+++ common/bigearth/TestSphereGeometry.java
@@ -4,18 +4,37 @@ public class TestSphereGeometry
 {
 	public static void main(String [] args)
 	{
-		Geometry g = new SphereGeometry(13);
-		System.out.println("Number of cells: " + g.getCellCount());
+		int gSize = Integer.parseInt(args[0]);
+		Geometry g = new SphereGeometry(gSize);
+		int numCells = g.getCellCount();
 
-		for (int i = 1, l = g.getCellCount(); i <= l; i++)
+		int numIters;
+		if (args.length>1)
+			numIters = Integer.parseInt(args[1]);
+		else
+			numIters = 200000000/numCells;
+
+
+		System.out.println("Size: " + gSize);
+		System.out.println("Number of cells: " + numCells);
+		System.out.println("Performing "+numIters + " iterations");
+
+		long startTime = System.currentTimeMillis();
+		long sum = 0;
+		for (int j = 0; j < numIters; j++)
 		{
-			int [] nn = g.getNeighbors(i);
-			System.out.print(i+": "+nn[0]);
-			for (int j = 1; j < nn.length; j++)
+			for (int i = 1; i <= numCells; i++)
 			{
-				System.out.print(", "+nn[j]);
+				int [] nn = g.getNeighbors(i);
+				sum += nn[0];
 			}
-			System.out.println();
 		}
+		long endTime = System.currentTimeMillis();
+
+		double t = (endTime - startTime);
+		t /= (numIters * numCells);
+
+		System.out.printf("Elapsed time: %d ms\n", endTime-startTime);
+		System.out.printf("Average time per getNeighbors() lookup : %.6f ms\n", t);
 	}
 }
