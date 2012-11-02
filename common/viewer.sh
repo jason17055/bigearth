5b8c6e92b665951f07acc2610b72c3def7e160c0
Jason Long <jason@long.name>
2012-11-01 21:33:08 -0400

    java: viewer.sh: convenience script for launching the world viewer

---
+++ common/viewer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+classpath_separator=";"
+classpath=$(
+	echo -n classes
+	find ../jlib -name '*.jar' |{ while read f; do
+		echo -n "$classpath_separator$f"
+	done; }
+	)
+
+java -cp "$classpath" -enableassertions bigearth.WorldViewer "$@"
