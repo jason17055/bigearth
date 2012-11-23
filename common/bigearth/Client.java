85f06ec9c65dcf1d435aa40cac97616ea142a050
Jason Long <jason@long.name>
2012-11-23 17:14:05 -0500

    Client: get so far as sending the 'login' request

--- common/bigearth/Client.java
+++ common/bigearth/Client.java
@@ -1,10 +1,64 @@
 package bigearth;
 
+import java.io.*;
+import java.net.*;
 import javax.swing.*;
 
 public class Client
 {
+	String host;
+	String user;
+	String pass;
+
+	static final int DEFAULT_PORT = 2626;
+
+	public Client(String host, String user, String pass)
+	{
+		this.host = host;
+		this.user = user;
+		this.pass = pass;
+	}
+
+	public Client(String host, String user, char [] pass)
+	{
+		this(host, user, new String(pass));
+	}
+
+	void login()
+		throws IOException
+	{
+		String myHost;
+		int port;
+
+		int colonIdx = host.indexOf(':');
+		if (colonIdx == -1)
+		{
+			myHost = host;
+			port = DEFAULT_PORT;
+		}
+		else
+		{
+			myHost = host.substring(0, colonIdx);
+			port = Integer.parseInt(host.substring(colonIdx+1));
+		}
+
+		URL url = new URL("http", myHost, port, "/login");
+		System.out.println(url);
+
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		OutputStream out = conn.getOutputStream();
+		String x = URLEncoder.encode(user, "UTF-8")
+			+ "&" + URLEncoder.encode(pass, "UTF-8");
+		out.write(x.getBytes("UTF-8"));
+		out.close();
+
+	}
+
 	public static void main(String [] args)
+		throws Exception
 	{
 		JTextField hostField = new JTextField();
 		hostField.setText("localhost");
@@ -27,6 +81,9 @@ public class Client
 		if (rv != JOptionPane.OK_OPTION)
 			return;
 
-
+		Client me = new Client(hostField.getText(),
+				userField.getText(),
+				passField.getPassword());
+		me.login();
 	}
 }
