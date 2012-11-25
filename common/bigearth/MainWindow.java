7cfc75b267a993157a6aca02496a2a4fa66a1c05
Jason Long <jason@long.name>
2012-11-24 19:20:02 -0500

    MainWindow: add a menu bar

--- common/bigearth/MainWindow.java
+++ common/bigearth/MainWindow.java
@@ -2,6 +2,7 @@ package bigearth;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class MainWindow extends JFrame
 {
@@ -14,9 +15,61 @@ public class MainWindow extends JFrame
 		view = new WorldView();
 		add(view, BorderLayout.CENTER);
 
+		initMenu();
+
 		pack();
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		setLocationRelativeTo(null);
+	}
+
+	void initMenu()
+	{
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu gameMenu = new JMenu("Game");
+		menuBar.add(gameMenu);
+
+		JMenuItem menuItem;
+		menuItem = new JMenuItem("Exit");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				onExitClicked();
+			}});
+		gameMenu.add(menuItem);
+
+		JMenu viewMenu = new JMenu("View");
+		menuBar.add(viewMenu);
+
+		menuItem = new JMenuItem("Zoom In");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				onZoomInClicked();
+			}});
+		viewMenu.add(menuItem);
+
+		menuItem = new JMenuItem("Zoom Out");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				onZoomOutClicked();
+			}});
+		viewMenu.add(menuItem);
+
+		setJMenuBar(menuBar);
+	}
+
+	private void onExitClicked()
+	{
+		dispose();
+	}
+
+	private void onZoomInClicked()
+	{
+		view.zoomIn();
+	}
+
+	private void onZoomOutClicked()
+	{
+		view.zoomOut();
 	}
 
 	public void setMap(MapModel map)
