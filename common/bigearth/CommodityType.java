3ac7a82ef6d0d8d5d07b480b5cd9f8fb12220916
Jason Long <jason@long.name>
2012-12-05 21:55:03 -0500

    commodities: define pigs and sheep

--- common/bigearth/CommodityType.java
+++ common/bigearth/CommodityType.java
@@ -2,14 +2,18 @@ package bigearth;
 
 public enum CommodityType
 {
-	MEAT(50.0, 800);
+	MEAT(50.0, 800, false),
+	SHEEP(80.0, 0, true),
+	PIG(60.0, 0, true);
 
 	public double mass;
 	public int nutrition;
+	public boolean isLivestock;
 
-	private CommodityType(double mass, int nutrition)
+	private CommodityType(double mass, int nutrition, boolean isLivestock)
 	{
 		this.mass = mass;
 		this.nutrition = nutrition;
+		this.isLivestock = isLivestock;
 	}
 }
