4e645bb09bba995d7a70b68b737b16e29f86951b
Jason Long <jlong@messiah.edu>
2012-12-09 14:12:22 -0500

    build.xml: create a build.xml file for building with Ant

---
+++ client.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+classpath_separator=";"
+classpath=$(
+	echo -n target/build
+	find extlib -name '*.jar' |{ while read f; do
+		echo -n "$classpath_separator$f"
+	done; }
+	)
+
+java -cp "$classpath" -enableassertions -Xshare:off bigearth.Client "$@"
