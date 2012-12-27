2c02cbd10ff9547628944e83cde43dea3206d869
Jason Long <jason@long.name>
2012-12-27 13:49:08 -0500

    server startup script: automatically make backups

--- server.sh
+++ server.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+worldname=world1
+
+ls world_backups | grep "^$worldname\\." | sort | head --lines=-2 |xargs -r -I X -- rm -rvf world_backups/X
+
+n=`date '+%Y%m%d.%H%M%S'`
+backupname="world_backups/$worldname.$n.tar.gz"
+echo "backing up $worldname to \`$backupname'"
+tar -czf $backupname $worldname
+
 classpath_separator=";"
 classpath=$(
 	echo -n target/build
