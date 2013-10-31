d1c436043c38998924622989c4f73b7f23fd8543
Jason Long <jason@long.name>
2013-10-30 20:32:06 -0400

    Cursor: document meaning of orientation

--- common/bigearth/geom/Cursor.java
+++ common/bigearth/geom/Cursor.java
@@ -6,7 +6,17 @@ package bigearth.geom;
  */
 public class Cursor
 {
+	/** Refers to a "face" of the polyhedron. */
 	public int location;
+
+	/** Identifies a side/corner of the selected face.
+	 * Ranges from 0 to n-1, where n is the number of sides
+	 * (or number of corners) of this face.
+	 * Increasing orientation represent moving counter-clockwise
+	 * around the face. The corner identified by a given
+	 * orientation is always the corner preceding the equivalent
+	 * side when moving counter-clockwise.
+	 */
 	public int orientation;
 
 	public Cursor()
